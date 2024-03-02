33cff3bb922abeb28c1ca79f42fb7261e9f26295
Ronan Keryell <ronan.keryell@amd.com>
2024-03-01 19:07:17 -0800

    Rewrite llvm.aie.put.ms to AIE++ emulator mangled name

--- lib/Dialect/AIE/Transforms/AIEUseAIEppEmulation.cpp
+++ lib/Dialect/AIE/Transforms/AIEUseAIEppEmulation.cpp
@@ -8,8 +8,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <regex>
-
 // #include "aie/Dialect/AIE/IR/AIEDialect.h"
 #include "aie/Dialect/AIE/Transforms/AIEPasses.h"
 
@@ -17,6 +15,7 @@
 #include "llvm/Support/Regex.h"
 #include "llvm/TargetParser/Host.h"
 
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/BuiltinOps.h"
 
@@ -26,7 +25,38 @@ namespace xilinx::AIE {
 
 namespace {
 
-void renameTileProgramFunctionsForEmulation(mlir::ModuleOp module) {
+/// Replace the AIE intrisics calls to call to AIE++ emulator functions mangled
+/// with column and row so they are unique
+void rewriteAIEIntrinsics(mlir::ModuleOp module, mlir::func::FuncOp f,
+                          llvm::StringRef col, llvm::StringRef row) {
+  constexpr auto AIEPP_PUT_MS_FORMAT{
+      "_Z25aie_tile_put_ms_intrinsicITnDaLi{0}ETnDaLi{1}EEvii"};
+  f->walk([&](mlir::func::CallOp c) {
+    if (c.getCallee() == "llvm.aie.put.ms") {
+      std::string aieppIntrinsicsName =
+          llvm::formatv(AIEPP_PUT_MS_FORMAT, col, row);
+      if (auto aieppIntrinsics =
+              module.lookupSymbol<mlir::func::FuncOp>(aieppIntrinsicsName);
+          !aieppIntrinsics) {
+        // Declare the intrinsics at the end of the module so the reader can
+        // focus on the important part first
+        auto b = mlir::OpBuilder::atBlockEnd(module.getBody());
+        aieppIntrinsics = b.create<mlir::func::FuncOp>(
+            b.getUnknownLoc(), aieppIntrinsicsName,
+            mlir::FunctionType::get(b.getContext(),
+                                    {b.getI32Type(), b.getI32Type()}, {}));
+        aieppIntrinsics.setPrivate();
+      }
+      c.setCallee(aieppIntrinsicsName);
+    }
+  });
+}
+
+/// Rewrite the core tile program functions to target AIE++
+///
+/// Rename the core tile program functions and AIE intrisics to have a mangled
+/// name compatible with AIE++ emulator.
+void rewriteTileProgramFunctionsForEmulation(mlir::ModuleOp module) {
   // The name format of a function representing a tile program
   static const llvm::Regex CORE_NAME{"^core_([[:digit:]]+)_([[:digit:]]+)$"};
   // The name format of the function inside AIE++ emulator to be formatted
@@ -41,6 +71,7 @@ void renameTileProgramFunctionsForEmulation(mlir::ModuleOp module) {
       const auto &row = matches[2];
       std::string name = llvm::formatv(AIEPP_EMULATOR_FORMAT, col, row);
       f.setName(name);
+      rewriteAIEIntrinsics(module, f, col, row);
     }
   });
 }
@@ -58,7 +89,7 @@ struct AIEUseAIEppEmulationPass
     module->setAttr(mlir::LLVM::LLVMDialect::getTargetTripleAttrName(),
                     builder.getStringAttr(llvm::sys::getDefaultTargetTriple()));
 
-    renameTileProgramFunctionsForEmulation(module);
+    rewriteTileProgramFunctionsForEmulation(module);
   }
 };
 
